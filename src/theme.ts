ee3269c33d49254e007866133a1c7f8cd8ea9863
Maisaa Hamza <maisaa.hamza66@gmail.com>
2024-01-18 13:36:55 +0200

    Update theme.ts

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,4 @@
-/** *********************************************************
- * This software and related documentation are proprietary
- * and confidential to Siemens.
- * Copyright 2023 Siemens.
- ********************************************************** */
+
 
 import { createTheme, Theme, ThemeOptions } from '@mui/material/styles';
 const colors = { 
